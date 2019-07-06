5f63d6368f4923f7079024076e528afd026fe4e0
prunkdump <prunkdump@gmail.com>
2019-07-06 09:51:18 +0200

    Added samba module common

---
+++ modules/samba/manifests/init.pp
@@ -0,0 +1,87 @@
+class samba (
+   $bind_module = $samba::params::bind_module,
+   $disable_nss = $samba::params::disable_nss,
+   $disable_pam = $samba::params::disable_pam,
+   $disable_groups = $samba::params::disable_groups,
+   $default_groups = $samba::params::default_groups,
+   $account_server = $samba::params::account_server,
+   $accountsrv_dns = $samba::params::accountsrv_dns,
+   $account_servers = $samba::params::account_servers,
+   $print_server = $samba::params::print_server,
+   $sysvolrsyncsrv_dns = $samba::params::sysvolrsyncsrv_dns,
+   $sysvolrsync_password = $samba::params::sysvolrsync_password,
+   $domain = $samba::params::domain,
+   $short_domain = $samba::params::short_domain,
+   $idmap_range = $samba::params::idmap_range,
+   $base_uid = $samba::params::base_uid,
+   $base_gid = $samba::params::base_gid,
+   $interfaces = $samba::params::interfaces,
+   $default_admin_pass = $samba::params::default_admin_pass,
+   $dns_forwarders = $samba::params::dns_forwarders,
+   $network = $samba::params::network,
+   $reverse_zone = $samba::params::reverse_zone,
+   $adservice = $samba::params::adservice,
+   $nmbdservice = $samba::params::nmbdservice,
+   $smbdservice = $samba::params::smbdservice,
+   $winbindservice = $samba::params::winbindservice,
+   $etc_path = $samba::params::etc_path,
+   $private_path = $samba::params::private_path,
+   $binddlz_path = $samba::params::binddlz_path,
+   $lib_path = $samba::params::lib_path,
+   $sysvol_path = $samba::params::sysvol_path,
+   $users_ou = $samba::params::users_ou,
+   $shares_ou = $samba::params::shares_ou,
+   $students_ou = $samba::params::students_ou,
+   $teachers_ou = $samba::params::teachers_ou,
+   $users_group = $samba::params::users_group,
+   $shares_group = $samba::params::shares_group,
+   $students_group = $samba::params::students_group,
+   $teachers_group = $samba::params::teachers_group,
+   $class_supervisor_suffix = $samba::params::class_supervisor_suffix,
+   $dfs_path = $samba::params::dfs_path,
+   $redirector = $samba::params::redirector,
+   $home_redirections = $samba::params::home_redirections,
+   $profile_redirections = $samba::params::profile_redirections,
+   $home_path = $samba::params::home_path,
+   $home_share = $samba::params::home_share,
+   $profile_path = $samba::params::profile_path,
+   $profile_share = $samba::params::profile_share,
+   $share_path = $samba::params::share_path,
+   $share_share = $samba::params::share_share,
+   $home_drive = $samba::params::home_drive,
+   $common_drive = $samba::params::common_drive,
+   $students_dir = $samba::params::students_dir,
+   $teachers_dir = $samba::params::teachers_dir,
+   $teachers_common_dir = $samba::params::teachers_common_dir,
+   $common_dir = $samba::params::common_dir,
+   $resource_dir = $samba::params::resource_dir, 
+   $shared_dir = $samba::params::shared_dir,
+   $test_dir = $samba::params::test_dir,
+   $common_mount_name = $samba::params::common_mount_name,
+   $login_min_length = $samba::params::login_min_length,
+   $login_surname_length = $samba::params::login_surname_length,
+   $login_givenname_length = $samba::params::login_givenname_length,
+   $quota_mount_point = $samba::params::quota_mount_point,
+   $quota_student_soft = $samba::params::quota_student_soft,
+   $quota_student_hard = $samba::params::quota_student_hard,
+   $quota_teacher_soft = $samba::params::quota_teacher_soft,
+   $quota_teacher_hard = $samba::params::quota_teacher_hard,
+   $maingpo_name = $samba::params::maingpo_name,
+   $maingpo_id = $samba::params::maingpo_id,
+   $maingpo_version = $samba::params::maingpo_version,
+   $maingpo_user_extensions = $samba::params::maingpo_user_extensions
+
+) inherits samba::params {
+
+   ########################
+   # computed common vars #
+   ########################
+
+   # realm #
+   $realm = upcase($domain)
+
+   # base_dn #
+   $base_dn_suffix = regsubst($domain, '\.', ',DC=', 'G')
+   $base_dn = "DC=${base_dn_suffix}" 
+
+}
---
+++ modules/samba/manifests/params.pp
@@ -0,0 +1,109 @@
+class samba::params {
+
+   include network
+
+   # enable/disable #
+   $bind_module = winbind 
+   $disable_nss = false
+   $disable_pam = false
+   $disable_groups = false
+   $default_groups = []
+   $account_server = false
+   $accountsrv_dns = 'sambaaccount'
+   $account_servers = []
+   $print_server = false
+   $sysvolrsyncsrv_dns = 'sysvolrsync' 
+   $sysvolrsync_password = 'us@se!sd$ac'
+
+   # network #
+   $domain = 'samdom.com'
+   $short_domain = 'SAMNET'
+   $idmap_range = '3000000-9999999'
+   $base_uid = '4000000'
+   $base_gid = '5000000'
+   $interfaces = ['lo','eth0']
+   $default_admin_pass = 'SambaAdmin@4'
+   $dns_forwarders = ["$::network::first_address"]
+   $network = $::network::slashform
+   $reverse_zone = $::network::reverse_zone
+ 
+   # distribution setup #
+   case $::osfamily {
+      'Debian': {
+         $adservice = 'samba-ad-dc'
+         $nmbdservice = 'nmbd'
+         $smbdservice = 'smbd'
+         $winbindservice = 'winbind'
+         $etc_path = '/etc/samba'
+         $private_path = '/var/lib/samba/private'
+         $binddlz_path = '/var/lib/samba/bind-dns'
+         $sysvol_path = '/var/lib/samba/sysvol'
+         $lib_path = $::architecture ? {
+            'amd64' => '/usr/lib/x86_64-linux-gnu/samba',
+            'i386' => '/usr/lib/i386-linux-gnu/samba',
+            default => '/usr/lib/samba',
+         }         
+      }
+      default: {
+         fail("The ${module_name} is not supported on an ${::osfamily} distribution")
+      }
+   }
+
+   # ldap structure #
+   $users_ou = 'S4users'
+   $shares_ou = 'S4shares'
+   $students_ou = 'Students'
+   $teachers_ou = 'Teachers'
+ 
+   # group structure #
+   $users_group = 's4users'
+   $shares_group = 's4shares'
+   $students_group = 'students'
+   $teachers_group = 'teachers'
+   $class_supervisor_suffix = 'supervisor'
+
+   # server share directories #
+   $dfs_path = '/srv/dfs'
+   $redirector = 'localhost'
+   $home_redirections = {}
+   $profile_redirections = {}
+   $home_path = '/home'
+   $home_share = 'homes'
+   $profile_path = '/profile'
+   $profile_share = 'profiles'
+   $share_path = '/share'
+   $share_share = 'shares'
+   $home_drive = 'H:'
+   $common_drive = 'P:'
+   $students_dir = 'students'
+   $teachers_dir = 'teachers'
+   $teachers_common_dir = '__Teachers__'
+   $common_dir = '__Common__'
+   $resource_dir = '__Resource__'
+   $shared_dir = '__Share__'
+   $test_dir = '__Tests__'
+   $common_mount_name = 'Common'
+
+   # accounts parameters #
+   $login_min_length = 2
+   $login_surname_length =  7
+   $login_givenname_length = 1
+
+   # quota #
+   $quota_mount_point = '/home'
+   $quota_student_soft = '1Go'
+   $quota_student_hard = '2Go'
+   $quota_teacher_soft = '5Go'
+   $quota_teacher_hard = '6Go'
+
+   # main gpo #
+   $maingpo_name = 'logonScript'
+   $maingpo_id = '{5D552AD7-B6FC-4D69-989D-D710FB5D9643}'
+   $maingpo_version = 262144
+
+   #extensions
+   #ProcessScriptsGroupPolicy and 
+   #Scripts (Logon/Logoff) Run Restriction
+   $maingpo_user_extensions = ['{42B5FAAE-6536-11D2-AE5A-0000F87571E3}',
+                               '{40B66650-4972-11D1-A7CA-0000F87571E3}'] 
+}
