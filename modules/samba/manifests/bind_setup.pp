c49b67a98f737dfcb196143c7365a3d18303afda
prunkdump <prunkdump@gmail.com>
2019-07-06 10:05:11 +0200

    Added bind setup module

---
+++ modules/samba/manifests/bind_setup.pp
@@ -0,0 +1,104 @@
+class samba::bind_setup (
+   $domain,
+   $network,
+   $dns_forwarders,
+   $private_path,
+   $binddlz_path,
+   $lib_path
+) {
+   ############
+   # packages #
+   ###########
+
+   package { ['bind9', 'ldb-tools']:
+      ensure => installed,
+   }
+
+   ###################
+   # set permissions #
+   ###################
+   
+   # set permission to bind #
+   # to samba files #
+   file { "$binddlz_path/dns":
+      ensure => directory,
+      owner => 'bind',
+      group => 'bind',
+      mode => '0775', 
+      require => Package['bind9'],
+   }
+
+   exec { 'samba_bind_dns_chown':
+      command => "chown -R bind:bind $binddlz_path/dns",
+      path => '/usr/bin:/usr/sbin:/bin',
+      subscribe => File["$binddlz_path/dns"],
+      refreshonly => true,
+      require => Package['bind9'],
+   }
+
+   file {"$binddlz_path/dns.keytab":
+      ensure => file,
+      group => 'bind',
+      mode => '0640',
+      require => Package['bind9'],
+   }
+
+   #############
+   # configure #
+   #############
+
+   file { '/etc/bind/named.conf.options':
+      ensure => file,
+      content => template('samba/named.conf.options.erb'),
+      mode => '0644',
+      require => Package['bind9'],
+   }
+
+   file_line { 'insert_bind_dlz' :
+      path => '/etc/bind/named.conf',
+      line => 'include "/etc/bind/named.conf.samba";',
+      after => '\s*include.*named\.conf\.local',
+      ensure => present,
+      multiple => false,
+      require => Package['bind9'],
+   }
+
+   # changed to include, this give the path to the DLZ module #
+   file { '/etc/bind/named.conf.samba' :
+      ensure => file,
+      content => "include \"$binddlz_path/named.conf\";",
+      mode => '0644',
+      require => Package['bind9'],
+   }
+
+   file { "$binddlz_path/named.conf" :
+      ensure => file,
+      owner => 'bind',
+      group => 'bind',
+      content => template('samba/named.conf.samba.erb'),
+      mode => '0644',
+      require => Package['bind9'],
+   }
+   
+   ##################
+   # dns transition #
+   ##################
+
+   # bind need to be restarted now ! #
+   service { 'bind9':
+      ensure => running,
+      enable => true,
+      subscribe => [File["$binddlz_path/dns","$binddlz_path/dns.keytab",
+                         '/etc/bind/named.conf.options','/etc/bind/named.conf.samba',"$binddlz_path/named.conf"],
+                   Exec['samba_private_dns_chown'],
+                   File_line['insert_bind_dlz']],
+   }
+  
+   # update resolv.conf #
+   file { '/etc/resolv.conf' :
+      ensure => file,
+      content => template('samba/resolv.conf.erb'),
+      mode => '0644',
+      require => Service['bind9'],
+   }
+}
---
+++ modules/samba/manifests/pdc/install.pp
@@ -0,0 +1,53 @@
+class samba::pdc::install {
+
+   $interfaces = $samba::pdc::interfaces
+   $realm = $samba::pdc::realm
+   $short_domain = $samba::pdc::short_domain
+   $default_admin_pass = $samba::pdc::default_admin_pass
+   $etc_path = $samba::pdc::etc_path
+   $private_path = $samba::pdc::private_path
+
+   #################
+   # main packages #
+   #################
+
+   # samba packages #
+   package { ['samba','winbind','acl','ntp']:
+      ensure => installed,
+   }
+
+   # kerberos packages #
+   package { krb5-user:
+      ensure => installed,
+   }
+
+   # quota tools #
+   package { ['quota','quotatool']:
+      ensure => installed,
+   }
+
+   ###################
+   # samba provision #
+   ###################
+
+   # provision samba #
+   $interfaces_flat = join($interfaces, ' ')
+   exec { 'samba4_provision':
+      path => '/usr/bin:/usr/sbin:/bin',
+      command => "rm -f $etc_path/smb.conf && \
+                  samba-tool domain provision --option=\"interfaces = $interfaces_flat\" \
+                  --option=\"bind interfaces only = yes\" --use-rfc2307 \
+                  --realm=$realm --domain=$short_domain \
+                  --adminpass=$default_admin_pass --server-role=dc --dns-backend=BIND9_DLZ",
+      creates => "$private_path/sam.ldb",
+      require => Package['samba'],
+   }
+
+   # set krb5 #
+   file { '/etc/krb5.conf':
+      ensure => present,
+      source => "file:$private_path/krb5.conf",
+      require => Exec['samba4_provision'],
+      mode => '0644',
+   }
+}
