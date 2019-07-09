74f94a1b2b2ef91a363d37526b27efeac78054d6
prunkdump <prunkdump@gmail.com>
2019-07-09 11:13:24 +0200

    Added base puppet module

---
+++ modules/puppet/manifests/init.pp
@@ -0,0 +1,15 @@
+class puppet (
+   $casrv_dns = $puppet::params::casrv_dns,
+   $puppetcarsync_password = $puppet::params::puppetcarsync_password, 
+   $mastersrv_dns = $puppet::params::mastersrv_dns,
+   $enable_fai = $puppet::params::enable_fai,
+   $fai_pxe_finename = $puppet::params::fai_pxe_finename,
+   $fai_loguser = $puppet::params::fai_loguser,
+   $fai_root_password = $puppet::params::fai_root_password,
+   $fai_debootstraps = $puppet::params::fai_debootstraps,
+   $fai_hosts = $puppet::params::fai_hosts
+) inherits puppet::params {
+
+
+
+}
---
+++ modules/puppet/manifests/params.pp
@@ -0,0 +1,12 @@
+class puppet::params {
+
+   $casrv_dns = 'puppetca'
+   $puppetcarsync_password = 'oi-wm)tr!yu' 
+   $mastersrv_dns = 'puppet'
+   $enable_fai = true 
+   $fai_pxe_finename = 'fai/pxelinux.0'
+   $fai_loguser = 'fai'
+   $fai_root_password = '$1$kBnWcO.E$djxB128U7dMkrltJHPf6d1' 
+   $fai_debootstraps = {}
+   $fai_hosts = {}
+}
