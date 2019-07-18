398c5e9ecd47de883c9d373cd76e44055d75d054
prunkdump <prunkdump@gmail.com>
2019-07-18 10:15:16 +0200

    disabled client modules

--- environments/production/manifests/nodes.pp
+++ environments/production/manifests/nodes.pp
@@ -18,78 +18,78 @@ node 'example_pdc.samdom.com' {
 }
 
 node 'example_dc.samdom.com' {
-   include stdlib
-   class { 'network': }
-   class { 'samba::dc':
-      disable_pam => true,
-   }
-   class { 'apt::server': }
-   class { 'puppet::master': }
-   class { 'debbug::sambaserver': }
+   #include stdlib
+   #class { 'network': }
+   #class { 'samba::dc':
+   #   disable_pam => true,
+   #}
+   #class { 'apt::server': }
+   #class { 'puppet::master': }
+   #class { 'debbug::sambaserver': }
 }
 
 
 node default {
 
-   include stdlib
+   #include stdlib
  
    #########
    # setup #
    #########
-   class { 'network':
-      stage => 'setup',
-   }
-   class { 'apt::client':
-      stage => 'setup',
-   }
-   class { 'puppet::client':
-      stage => 'setup',
-      require => Class['apt::client'],
-   }
-   class { 'concat::setup':
-      stage => 'setup',
-      require => Class['apt::client'],
-   }
-   class { ['samba','samba::member']:
-      stage => 'setup',
-      require => Class['apt::client'],
-   }
-   class { ['pammount','nfs','nfs::client','nfs::client::dnsfmount']:
-      stage => 'setup',
-      require => Class['apt::client'],
-   }
-   class { 'wine::client::setup':
-      stage => 'setup',
-   }
+   #class { 'network':
+   #   stage => 'setup',
+   #}
+   #class { 'apt::client':
+   #   stage => 'setup',
+   #}
+   #class { 'puppet::client':
+   #   stage => 'setup',
+   #   require => Class['apt::client'],
+   #}
+   #class { 'concat::setup':
+   #   stage => 'setup',
+   #   require => Class['apt::client'],
+   #}
+   #class { ['samba','samba::member']:
+   #   stage => 'setup',
+   #   require => Class['apt::client'],
+   #}
+   #class { ['pammount','nfs','nfs::client','nfs::client::dnsfmount']:
+   #   stage => 'setup',
+   #   require => Class['apt::client'],
+   #}
+   #class { 'wine::client::setup':
+   #   stage => 'setup',
+   #}
  
    ########
    # main #
    ######## 
-   class { 'wifi': }
-   class { 'desktop': }
-   class { 'cups': }
-   #class { 'check-quota': }
-   class { 'keyboard': }
-   class { 'shutdown': }
-   class { 'mozilla':
-      require => Class['desktop'],
-   }
-   class { 'virt': }
-   class { 'localization': }
+   #class { 'wifi': }
+   #class { 'desktop': }
+   #class { 'cups': }
+   ##class { 'check-quota': }
+   #class { 'keyboard': }
+   #class { 'shutdown': }
+   #class { 'mozilla':
+   #   require => Class['desktop'],
+   #}
+   #class { 'virt': }
+   #class { 'localization': }
 
    ###########
    # runtime #
    ###########
-   class { 'hostpkg':
-      stage => 'runtime',
-   }
-   class { 'extrapkg':
-      stage => 'runtime',
-   }
-   class { 'wine::client':
-      stage => 'runtime',
-   }
-   class { 'debbug::client':
-      stage => 'runtime',
-   }
+   #class { 'hostpkg':
+   #   stage => 'runtime',
+   #}
+   #class { 'extrapkg':
+   #   stage => 'runtime',
+   #}
+   #class { 'wine::client':
+   #   stage => 'runtime',
+   #}
+   #class { 'debbug::client':
+   #   stage => 'runtime',
+   #}
 }
