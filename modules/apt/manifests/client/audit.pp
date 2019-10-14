9a1e07863fcd9adf0df721144b7e4adafec80af1
prunkdump <prunkdump@gmail.com>
2019-10-14 15:09:57 +0200

    apt : update audit command

--- modules/apt/manifests/client/audit.pp
+++ modules/apt/manifests/client/audit.pp
@@ -12,7 +12,7 @@ class apt::client::audit {
    # audit apt status #
    exec { 'apt_configure_partially':
       path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      command => 'apt-get update && apt-get clean && ( dpkg --configure -a || apt-get -f -y install )',
+      command => 'apt-get update && apt-get clean && ( dpkg --configure -a || apt-get -f -y install || apt --fix-broken install )',
       onlyif => 'dpkg --audit | grep -q .',
       require => File['01conffiles'],
    }
