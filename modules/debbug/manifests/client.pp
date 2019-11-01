49d8f0ba88693a1b2336e027cb8a38646afb796c
prunkdump <prunkdump@gmail.com>
2019-11-01 12:50:00 +0100

    debbug : dhclient timer not updated on suspend

--- modules/debbug/manifests/client.pp
+++ modules/debbug/manifests/client.pp
@@ -24,24 +24,51 @@ class debbug::client {
    }
 
 
-   # BUG winbind fail on suspend: dns lookup don't works on resume #
-   file { '/lib/systemd/system/sleep-winbind.service':
+   # BUG winbind fail on suspend #
+   # BUG dhclient does not update timers on suspend #
+   # BUG ntp dhclient exit hook make winbind failed on DHCPDISCOVER #
+   file { '/lib/systemd/system/suspend-bugs.service':
       ensure => present,
-      source => 'puppet:///modules/debbug/sleep-winbind.service',
+      source => 'puppet:///modules/debbug/suspend-bugs.service',
       mode => '0644',
    }
 
-   service { 'sleep-winbind':
-      enable => true,
-      require => File['/lib/systemd/system/sleep-winbind.service'],
+   file { '/sbin/dhclient-exit':
+      ensure => present,
+      source => 'puppet:///modules/debbug/dhclient-exit',
+      mode => '0755',
    }
 
-   exec { 'winbind_reload_systemd':
+   file_option { 'add_networking_reload':
+      path => '/lib/systemd/system/networking.service',
+      option => 'ExecReload',
+      value => '/sbin/ifup --force -a --read-environment',
+      separator => '=',
+      after => 'ExecStop',
+      multiple => false,
+      ensure => present,
+   }
+
+   exec { 'networking-service-daemon-reload':
+      path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      command => 'systemctl daemon-reload',
+      refreshonly => true,
+      subscribe => File_option['add_networking_reload'],
+   }
+
+   service { 'suspend-bugs':
+      enable => true,
+      require => [File['/lib/systemd/system/sleep-winbind.service','/sbin/dhclient-exit'],
+                  File_option['add_networking_reload'],
+                  Exec['networking-service-daemon-reload'],
+   }
+
+   exec { 'winbind_suspend_bug_restart':
       path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       command => 'systemctl restart winbind',
       refreshonly => true,
-      require => Service['sleep-winbind'],
-      subscribe => File['/lib/systemd/system/sleep-winbind.service'],
+      require => Service['suspend-bugs'],
+      subscribe => File['/lib/systemd/system/suspend-bugs.service'],
    }
 
    # nullmailer can fill disk with certain cron scripts #
