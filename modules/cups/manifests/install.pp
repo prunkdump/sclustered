3f862a74c3fdf27288b33a0e6d7c4893e2798d96
prunkdump <prunkdump@gmail.com>
2021-06-24 11:11:53 +0200

    cups : added server migration script

--- modules/cups/manifests/install.pp
+++ modules/cups/manifests/install.pp
@@ -8,4 +8,11 @@ class cups::install {
       ensure => installed,
    }
 
+   # script to migrate printers #
+   file { 'cups-migrate':
+      path => '/usr/bin/cups-migrate',
+      ensure => file,
+      source => 'puppet:///modules/cups/cups-migrate',
+      mode => '0755',
+   }
 }
