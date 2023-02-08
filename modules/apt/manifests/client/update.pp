184437771b54f86e00ffcf478b0a739dcb00cb72
prunkdump <prunkdump@gmail.com>
2023-02-08 12:34:37 +0100

    apt : prevent suspend while dist-upgrading

--- modules/apt/manifests/client/update.pp
+++ modules/apt/manifests/client/update.pp
@@ -6,6 +6,8 @@ class apt::client::update {
    exec { 'apt-get update':
       path => '/usr/bin:/usr/sbin:/bin',
       refreshonly => true,
+      tries => '10',
+      try_sleep => '1',
    }
 
    # prevent restarting puppet on upgrade #
@@ -24,9 +26,11 @@ class apt::client::update {
    # update all packages except puppet, will be upgraded with unattended upgrades #
    exec { 'debian_automated_distupgrade':
       path => '/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
-      command => "echo 'puppet hold' | dpkg --set-selections && env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get dist-upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' && echo 'puppet install' | dpkg --set-selections",
-      require => [Exec['apt-get update'],File['/usr/sbin/policy-rc.d'],Package['lsb-release']],
+      command => "systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target && env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get update -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' && echo 'puppet hold' | dpkg --set-selections && env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' && env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get dist-upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' && env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get dist-upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' &&  echo 'puppet install' | dpkg --set-selections && shutdown -h +4 && systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target",
       unless => "lsb_release -c | grep $distribution",
       timeout => 0,
+      tries => '60',
+      try_sleep => '60',
+      require => [Exec['apt-get update'],File['/usr/sbin/policy-rc.d'],Package['lsb-release']],
    }
 }
